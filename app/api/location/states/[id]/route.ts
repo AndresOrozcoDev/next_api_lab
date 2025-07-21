cc11b1407f06cd818fca9ed7f1cdd693610c1873
Andres Orozco Dev <andres.orozco.dev@gmail.com>
2025-07-21 01:47:32 -0400

    add

--- app/api/location/states/[id]/route.ts
+++ app/api/location/states/[id]/route.ts
@@ -1,15 +1,23 @@
 import { getCitiesByStateId } from '@/app/lib/location.service';
 import { NextRequest, NextResponse } from 'next/server';
 
-export async function GET(request: NextRequest, { params }: { params: { id: string } }
+export async function GET(_req: NextRequest, { params }: { params: { id: string } }
 ) {
-  const id = params.id;
-  const cities = await getCitiesByStateId(id);
+  try {
+    const cities = await getCitiesByStateId(params.id);
 
-  return NextResponse.json({
-    status_code: 200,
-    message: 'success',
-    data: cities,
-  });
+    return NextResponse.json({
+      status_code: 200,
+      message: 'success',
+      data: cities,
+    });
+  } catch (error) {
+    return NextResponse.json(
+      {
+        status_code: 500,
+        message: 'Internal server error',
+      },
+      { status: 500 }
+    );
+  }
 }
-
